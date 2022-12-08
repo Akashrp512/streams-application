78d43a39c8e6c75a33224971d92ad2aca1366b89
Akash RP <akashrp512@gmail.com>
2022-12-08 17:14:35 +0530

    commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,29 @@
-
+import React from 'react';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import StreamCreate from './components/streams/StreamCreate';
+import StreamShow from './components/streams/StreamShow';
+import StreamList from './components/streams/StreamList';
+import StreamEdit from './components/streams/StreamEdit';
+import StreamDelete from './components/streams/StreamDelete';
+import Header from './components/Header';
 
 function App() {
   return (
-    <div className="App">
-      App
-    </div>
+    <div className='ui container'>
+      
+      <BrowserRouter>
+      <Routes>
+          
+          <Route path='/*' element={<Header />}/>
+          <Route path='/' element= {<StreamList />}/>
+          <Route path='/streams/new' element= {<StreamCreate />}/>
+          <Route path='/streams/edit' element= {<StreamEdit />}/>
+          <Route path='/streams/delete' element= {<StreamDelete />}/>
+          <Route path='/streams/show' element= {<StreamShow />}/>
+      </Routes>
+      </BrowserRouter>
+      </div>
   );
 }
 
